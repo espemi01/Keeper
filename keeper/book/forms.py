73d4133bd17315d2f871014a86a6ba0eca9b781f
Michael Espey <espemi01@luther.edu>
2014-12-15 22:37:40 -0600

    Working version

--- keeper/book/forms.py
+++ keeper/book/forms.py
@@ -2,14 +2,17 @@ from flask.ext.wtf import Form
 from wtforms import fields
 from wtforms.validators import Required
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-
-from .models import Group
+from flask.ext.login import current_user
+from .models import Group, Contact
+from flask import Blueprint
 
 class GroupForm(Form):
 	name = fields.StringField(validators=[Required()])
 
 class ContactForm(Form):
-	name = fields.StringField(validators=[Required()], query_factory=lambda: Contact.query.all())
+	# group = QuerySelectField(query_factory=Group.query.filter(g.user_id == user.id))
+
+	name = fields.StringField(validators=[Required()])
 	phone = fields.StringField(validators=[Required()])
 	address = fields.TextField('', validators=[Required()])
 	city = fields.TextField('', validators=[Required()])
